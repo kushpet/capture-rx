40daa07a0524e81c446350131bfb7eb080bfa509
vivado <vivado@vivadopc.home>
2024-12-03 16:23:07 +0300

    Гружу осмысленные параметры и сохраняю в файл.

--- main.cpp
+++ main.cpp
@@ -1,28 +1,62 @@
 #include <iio.h>
 #include <cstdlib>
 #include <iostream>
+#include <fstream>
+#include <signal.h>
 
 int receive(struct iio_context *ctx);
+
+bool run = true;
+
+static void handle_sig(int sig)
+{
+	printf("Waiting for process to finish... Got signal %d\n", sig);
+	run = false;
+}
+
+
  
 int main (int argc, char **argv)
 {
 	struct iio_context *ctx;
 	struct iio_device *phy;
+
+	signal(SIGINT, handle_sig);
  
 	ctx = iio_create_context_from_uri("ip:192.168.2.1");
  
-	phy = iio_context_find_device(ctx, "ad9361-phy");
- 
+	phy = iio_context_find_device(ctx, "ad9361-phy"); //iio:device в iio_info
+	// Названия атрибутов берём также из вывода iio_info
 	iio_channel_attr_write_longlong(
-		iio_device_find_channel(phy, "altvoltage0", true),
+		iio_device_find_channel(phy, "altvoltage0", true), // RX_LO (output)
 		"frequency",
-		2400000000); /* RX LO frequency 2.4GHz */
+		1575420000);
  
 	iio_channel_attr_write_longlong(
-		iio_device_find_channel(phy, "voltage0", false),
+		iio_device_find_channel(phy, "voltage0", false), // (input)
+		"rf_bandwidth",
+		2600000);
+
+	iio_channel_attr_write_longlong(
+		iio_device_find_channel(phy, "voltage0", false), // (input)
 		"sampling_frequency",
-		5000000); /* RX baseband rate 5 MSPS */
+		3000000);
  
+	iio_channel_attr_write(
+		iio_device_find_channel(phy, "voltage0", false), // (input)
+		"rf_port_select",
+		"A_BALANCED");
+
+	iio_channel_attr_write(
+		iio_device_find_channel(phy, "voltage0", false), // (input)
+		"gain_control_mode",
+		"manual");
+
+	iio_channel_attr_write_double(
+		iio_device_find_channel(phy, "voltage0", false), // (input)
+		"hardwaregain",
+		71.0);
+
 	receive(ctx);
  
 	iio_context_destroy(ctx);
@@ -32,6 +66,12 @@ int main (int argc, char **argv)
 
 int receive(struct iio_context *ctx)
 {
+	std::ofstream out_data("samples.dat", std::ios::out | std::ios::binary);
+	if(!out_data){
+		std::cout << "Cannot open file samples.dat" << std::endl;
+		return 1;
+	}
+
 	struct iio_device *dev;
 	struct iio_channel *rx0_i, *rx0_q;
 	struct iio_buffer *rxbuf;
@@ -44,32 +84,38 @@ int receive(struct iio_context *ctx)
 	iio_channel_enable(rx0_i);
 	iio_channel_enable(rx0_q);
  
-	rxbuf = iio_device_create_buffer(dev, 4096, false);
+	rxbuf = iio_device_create_buffer(dev, 1024*1024, false);
 	if (!rxbuf) {
 		std::cerr << "Could not create RX buffer";
 		std::exit(-1);
 	}
  
-	while (true) {
-		void *p_dat, *p_end, *t_dat;
+	while (run) {
+		char *p_dat, *p_end, *t_dat;
 		ptrdiff_t p_inc;
  
 		iio_buffer_refill(rxbuf);
  
 		p_inc = iio_buffer_step(rxbuf);
-		p_end = iio_buffer_end(rxbuf);
+		p_end = (char*)iio_buffer_end(rxbuf);
+
+		p_dat = (char*)iio_buffer_first(rxbuf, rx0_i);
+
+		out_data.write(p_dat, p_end - p_dat);
  
-		for (p_dat = iio_buffer_first(rxbuf, rx0_i); p_dat < p_end; p_dat += p_inc, t_dat += p_inc) {
-			const int16_t i = ((int16_t*)p_dat)[0]; // Real (I)
-			const int16_t q = ((int16_t*)p_dat)[1]; // Imag (Q)
+//		for (p_dat = (char*)iio_buffer_first(rxbuf, rx0_i); p_dat < p_end; p_dat += p_inc, t_dat += p_inc) {
+//			const int16_t i = ((int16_t*)p_dat)[0]; // Real (I)
+//			const int16_t q = ((int16_t*)p_dat)[1]; // Imag (Q)
  
 			/* Process here */
  
-		}
+//		}
 	}
- 
+	out_data.close();
+
 	iio_buffer_destroy(rxbuf);
  
+	return 0;
 }
 
 
